7a8f9d13d4b44f63c8f8c3fe37289205ec7d8582
Obetta <francischassobetta@gmail.com>
2021-09-14 06:30:01 +0100

    Remove unnecessary file and setup project structure

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import styles from '../assets/css/App.module.css';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1>Hello CRUD</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
